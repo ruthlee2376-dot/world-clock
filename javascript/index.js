4dd758fc76ccea9b6f01f6e0962f144c26461548
ruthlee2376-dot <ruthlee2376@gmail.com>
2026-01-21 21:07:39 +0000

    Added link to all cities

--- javascript/index.js
+++ javascript/index.js
@@ -41,7 +41,9 @@ function updateCity(event) {
   <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
   </div>
   <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
-  </div>`;
+  </div>
+  <a href="/">Reload Page</a>
+  `;
 }
 
 updateTime();
